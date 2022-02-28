6a7617209be3d6f59855029d1e9f2a73b2004a52
Saray Gallardo <saray.gallardo@babel.es>
2022-02-28 02:51:22 +0100

    Splash screen logic

--- app/src/main/java/com/example/crewmanagement/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/crewmanagement/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.crewmanagement
+package com.example.crewmanagement.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.crewmanagement.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

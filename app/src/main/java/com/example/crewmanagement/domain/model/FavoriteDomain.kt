32480f51b65ec638a775ed77e09543eba20353ad
Saray Gallardo <saray.gallardo@babel.es>
2022-02-28 17:52:15 +0100

    Main Screen logic

--- app/src/main/java/com/example/crewmanagement/domain/model/FavoriteDomain.kt
+++ app/src/main/java/com/example/crewmanagement/domain/model/FavoriteDomain.kt
@@ -3,9 +3,9 @@ package com.example.crewmanagement.domain.model
 import java.io.Serializable
 
 data class FavoriteDomain(
-    private val color: String?,
-    private val food: String?,
-    private val randomString: String?,
-    private val song: String?
-): Serializable {
+    val color: String?,
+    val food: String?,
+    val randomString: String?,
+    val song: String?
+) : Serializable {
 }

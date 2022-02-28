2a4c7d00f0bc5deeaefd995094d6b9c657385fa8
Saray Gallardo <saray.gallardo@babel.es>
2022-02-28 21:04:45 +0100

    Detail screen logic

--- app/src/main/java/com/example/crewmanagement/presentation/ui/detail/DetailActivityViewModel.kt
+++ app/src/main/java/com/example/crewmanagement/presentation/ui/detail/DetailActivityViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.crewmanagement.presentation.ui.main
+package com.example.crewmanagement.presentation.ui.detail
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,7 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.crewmanagement.data.ErrorHandler.DEFAULT_ERROR
 import com.example.crewmanagement.domain.model.OompaLoompaDomain
 import com.example.crewmanagement.domain.model.OompaLoompaListDomainResponse
-import com.example.crewmanagement.domain.usecase.GetAllUseCase
+import com.example.crewmanagement.domain.usecase.GetDetailUseCase
 import com.example.crewmanagement.presentation.base.BaseViewModel
 import com.example.crewmanagement.presentation.base.FailureResource
 import com.example.crewmanagement.presentation.base.LoadingResource
@@ -15,30 +15,30 @@ import com.example.crewmanagement.presentation.base.SuccessResource
 import com.example.crewmanagement.utils.SingleEvent
 import kotlinx.coroutines.launch
 
-class MainActivityViewModel(
-    private val getAllUseCase: GetAllUseCase
+class DetailActivityViewModel(
+    private val getDetailUseCase: GetDetailUseCase
 ): BaseViewModel() {
 
-    private val _getAllLiveData = MutableLiveData<ResultResource<OompaLoompaListDomainResponse>>()
-    val getAllLiveData get() = _getAllLiveData as LiveData<ResultResource<OompaLoompaListDomainResponse>>
+    private val _getDetailLiveData = MutableLiveData<ResultResource<OompaLoompaDomain>>()
+    val getDetailLiveData get() = _getDetailLiveData as LiveData<ResultResource<OompaLoompaDomain>>
+
+    private val _oompaLoompaSelected = MutableLiveData<OompaLoompaDomain>()
+    val oompaLoompaSelected get() = _oompaLoompaSelected as LiveData<OompaLoompaDomain>
 
     private val _showToastLiveData = MutableLiveData<SingleEvent<Any>>()
     val showToastLiveData get() = _showToastLiveData as LiveData<SingleEvent<Any>>
 
-    private val _openDetailLiveData = MutableLiveData<SingleEvent<OompaLoompaDomain>>()
-    val openDetailLiveData get() = _openDetailLiveData as LiveData<SingleEvent<OompaLoompaDomain>>
-
-    fun openOompaLoompaDetail(oompaLoompa: OompaLoompaDomain) {
-        _openDetailLiveData.value = SingleEvent(oompaLoompa)
+    fun initIntentData(oompaLoompa: OompaLoompaDomain) {
+        _oompaLoompaSelected.value = oompaLoompa
     }
 
-    fun getAll() {
-        _getAllLiveData.postValue(LoadingResource())
+    fun getDetail(id: Int) {
+        _getDetailLiveData.postValue(LoadingResource())
         viewModelScope.launch {
             kotlin.runCatching {
-                getAllUseCase.execute(Unit).let { _getAllLiveData.postValue(SuccessResource(it))}
+                getDetailUseCase.execute(id).let { _getDetailLiveData.postValue(SuccessResource(it))}
             }.onFailure {
-                this@MainActivityViewModel._getAllLiveData.postValue(FailureResource(DEFAULT_ERROR))
+                this@DetailActivityViewModel._getDetailLiveData.postValue(FailureResource(DEFAULT_ERROR))
             }
         }
     }
